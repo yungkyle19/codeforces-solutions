b156285a8149b7a7b4f08fba276fb2f32b356c3f
NottCurious <56356841+NottCurious@users.noreply.github.com>
2020-10-18 19:02:00 +0530

    Week 4

---
+++ 133A.cpp
@@ -0,0 +1,19 @@
+// Written By Deepesh Nimma
+// Problem: 133A Codeforces
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    if (s.find_first_of("HQ9") != string::npos)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
